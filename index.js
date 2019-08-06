515458742e1bfefe799c4d80daf3ba4068a0a46b
Thomas Doyle <thomasdoyle@MacBook-Pro.outfit>
2019-08-06 13:04:43 +1000

    update package.json with script for deploy \nupdate index.js with working credentials\nupdate index.js with export.handler

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ var https = require('https');
 var AWS = require('aws-sdk');
 var params, body = {};
 
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default', filename: '/Users/thomasdoyle/.aws/credentials'});
+var credentials = new AWS.SharedIniFileCredentials({
+    profile: 'default',
+    filename: '/Users/thomasdoyle/.aws/credentials'
+});
 AWS.config.credentials = credentials;
-AWS.config.update({region: 'ap-southeast-2'});
+AWS.config.update({
+    region: 'ap-southeast-2'
+});
 
 var s3 = new AWS.S3();
 
@@ -13,13 +18,25 @@ params = {
     Bucket: "bulk-journey-beyond"
 };
 
-body = {
-    "tagName": ""
-}
+exports.handler = (event, context, callback) => {
+    params = {
+        Bucket: "bulk-journey-beyond"
+    };
+    // s3.getObject(params, function (err, data) {
+    s3.listObjectsV2(params, function (err, data) {
+        if (err) {
+            callback(err, null);
+        } else {
+            let response = {
+                "statusCode": 200,
+                "headers": {
+                    "my_header": "my_value"
+                },
+                "body": JSON.stringify(data),
+                "isBase64Encoded": false
+            };
+            callback(null, response);
+        }
+    });
 
-s3.listObjectsV2(params, function(err, data) {
-    if (err)    console.log(err, err.stack);
-    else        console.log(data);
-});
-
-
+};
